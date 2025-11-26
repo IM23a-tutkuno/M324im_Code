a847d5e3aa2f3fa9aec97e0fa8c53130ab0d0f5b
IM23a-tutkuno <tutkuno@bzz.ch>
2025-11-26 15:19:27 +0100

    initial commit

---
+++ 02/00_Demo/app/trafficlight.test.js
@@ -0,0 +1,22 @@
+//import module traffic ligh
+const app = require('./trafficlight');
+
+
+
+describe('Traffic Light Module', () => {
+
+    it('case red light', () => {
+        expect(app.trafficLight('red'))
+        .toBe("DON'T WALK!")
+    })
+})
+//??
+
+//test suite
+//??
+    //case red light
+//??
+//??
+//??
+//??
+//??
